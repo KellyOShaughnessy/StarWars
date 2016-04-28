5c01e8d7edb9c27b44fbbe92214348b49221a11d
Adi Bodas <asb326@cornell.edu>
2016-04-28 19:29:48 -0400

    finished orbits

--- main.js
+++ main.js
@@ -34,6 +34,15 @@ loop.start();
 
 //Dropdown menu functions
 
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined' ? args[number] : match;
+    });
+  };
+}
+
 function drop() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -73,7 +82,7 @@ function graphics(ep) {
 	    RandCSV.forEach(function(planetRow, i) {
 	    	//Get pertinent data
 	      if (planetRow["PLANET"] != null) {
-	      	var episodes = planetRow["FILMS (EP)"].split(", ");
+	      	var episodes = planetRow["FILMS (EP)"].split(",");
 	      	//planet in this episode; ep = 0 if all episodes
 	      	if (ep == 0 || episodes.indexOf(ep) != -1) {
 		        dataset.planets.push({
@@ -85,7 +94,7 @@ function graphics(ep) {
 		        	url: planetRow["IMAGE"],
 		        	color: color2[i]
 		        });
-			}
+					}
 	      }
 	    });
 	    //Turn planet type into number
@@ -253,6 +262,216 @@ function graphics(ep) {
 		.attr("font-size","14")
 		.text("Planet populations")
 		.attr("text-anchor", "middle");
+	});
+}
+
+function populate_planets(selectedFilm) {
+	var PLANET_STATE = {
+		ORBIT : 0,
+		HOVER : 1,
+		SELECT : 2
+	}
+
+	var t0 = Date.now(),
+	  	planets = [],
+		 	width = 2000,
+			height = 2000,
+			padding = 50,
+			mid = {
+	      x: width / 2,
+	      y: height / 2
+	    },
+	  	circle = [],
+	  	selected = null;
+
+	var svg = d3.select("#svg").append("svg")
+	  .attr("height", height)
+	  .attr("width", width)
+	  .attr("fill", "black");
+
+	var planetInfo1 = svg.append("text").attr("class", "info").attr("x", width/1.4)
+	  .attr("y", height/2.3)
+	  .attr("fill", "white")
+	  .attr("visibility", "visible")
+	.style("font-size", "26px");
+
+	var planetInfo2 = svg.append("text").attr("class", "info").attr("x", width/1.4)
+	  .attr("y", height/2.2)
+	  .attr("fill", "white")
+	  .attr("visibility", "visible")
+	.style("font-size", "26px");
+
+	var planetInfo3 = svg.append("text").attr("class", "info").attr("x", width/1.4)
+	  .attr("y", height/2.1)
+	  .attr("fill", "white")
+	  .attr("visibility", "visible")
+	.style("font-size", "26px");
+
+	var planetInfo4 = svg.append("text").attr("class", "info").attr("x", width/1.4)
+	  .attr("y", height/2)
+	  .attr("fill", "white")
+	  .attr("visibility", "visible")
+	.style("font-size", "26px");
+
+	var planetInfo5 = svg.append("text").attr("class", "info").attr("x", width/1.4)
+	.attr("y", height/2.5)
+	.attr("fill", "white")
+	.attr("visibility", "visible")
+	.style("font-size", "42px");
+
+
+	d3.csv("planet_info.csv", function(planetsCsv) {
+
+	  var orbitSpeedScale = d3.scale.linear().domain([0,1]).range([.2,.7]);
+	  		orbitScale 			= d3.scale.pow().domain([0,1]).range([50, 750])
+
+	  planetsCsv.forEach(function(planetRow, i) {
+	    if (planetRow["PLANET"] != null && planetRow["PLANET"] != "Earth") {
+	    	var films = planetRow["FILMS (EP)"].split(',').map(function(x) { return +x; });
+	    	if (selectedFilm == 0 || films.includes(selectedFilm)) {
+	    		var orbit = orbitScale(Math.random());
+		      planets.push({
+		        orbitSpeed: orbitSpeedScale(Math.random()),
+		        orbit: orbit,
+		        radius: 100, //EDIT: 25
+		        delta: orbit * 50,
+		        state: PLANET_STATE.ORBIT,
+		        opacity: 1,
+		        //offset: planetRow["OFFSET"],
+		        id: planetRow["PLANET"],
+		  		  climate: planetRow["CLIMATE"],
+		  		  population: planetRow["POPULATION"],
+		  		  type: planetRow["TYPE"],
+		  		  size: planetRow["DIAMETER"],
+		        url: planetRow["IMAGE"]
+		      });
+	    	}
+	    }
+	  })
+
+	  var planetName = svg.append("text")
+	  	.attr("x", padding).attr("y", padding)
+	  	.style("fill", "white")
+	  	.style("font-size", "50")
+	  	.attr("visibility", "hidden");
+
+	  svg.selectAll("circle").data(planets)
+	  	 .enter()
+	  	 .append("circle")
+	  	 .attr("class", "orbit")
+	  	 .attr("cx", mid.x)
+	  	 .attr("cy", mid.y)
+		   .style("vertical-align", "middle")
+		   .style("text-align", "center")
+		   .attr("r", function(d) {
+		   	return d.orbit + d.radius / 2;
+		   })
+		   .style("fill", "none")
+		   .style("stroke", "white")
+		   .style("stroke-width", 4)
+		   .style("opacity", 0.2);
+
+	  svg.selectAll("image").data(planets)
+	    .enter()
+	    .append("image")
+	    .attr("class", "planet")
+	    .attr("id", planets.id)
+	    //.attr("r", 20)  //radius is scaled populationSize
+	    .style("vertical-align", "middle")
+	    .style("text-align", "center")
+	    .attr("height", function(d) {
+	      return d.radius;
+	    })
+	    .attr("width", function(d) {
+	      return d.radius;
+	    })
+	    .attr("x", function(d) {
+	      return (mid.x + d.orbit /*+ d.offset*/ );
+	    })
+	    .attr("y", function(d) {
+	      return (mid.y /*+ d.offset*/ );
+	    })
+	    .attr("opacity", function(d) {
+	      return d.opacity;
+	    })
+	    .attr("xlink:href", function(d) {
+	      return ("img/" + d.url);
+	    })
+	    .on("mouseover", function(d) {
+	      d.state = d.state == PLANET_STATE.SELECT ? 
+	      					d.state = PLANET_STATE.SELECT :
+	      					d.state = PLANET_STATE.HOVER;
+
+	     	planetName.attr("visibility", "visible").text(d.id);
+
+	    })
+	    .on("mouseout", function(d) {
+	      d.state = d.state == PLANET_STATE.SELECT ? 
+	      					d.state = PLANET_STATE.SELECT :
+	      					d.state = PLANET_STATE.ORBIT;
+	      planetName.attr("visibility", "hidden").text("");
+	    })
+	    .on("click", function(d) {
+	    	if (d.state == PLANET_STATE.SELECT) {
+	  			selected = null;
+	  			d.state = PLANET_STATE.ORBIT;
+					d3.selectAll(".info").text("").attr("visibility", "hidden");
+	    	} else {
+	  		  if (selected) 
+	  		 		selected.state = PLANET_STATE.ORBIT;
+	  		 	selected = d;
+	  		 	d.state = PLANET_STATE.SELECT;
+				
+					planetInfo5.text(d.id);
+					planetInfo1.text("PLANET TYPE: "+d.type);
+					planetInfo2.text("SIZE: "+d.size+" km")
+					planetInfo3.text("CLIMATE: "+d.climate)
+					planetInfo4.text("POPULATION: "+d.population);
+					d3.selectAll(".info").attr("visibility", "visible");
+	    	}     
+	    });
+
+	  d3.timer(function() {
+	    var circles = svg.selectAll(".planet");
+	    //stops rotating on hover or click
+	    circles.attr("transform", function(d) {
+	        d.delta += d.state == PLANET_STATE.ORBIT ? d.orbitSpeed : 0;
+	        return d.state == PLANET_STATE.SELECT ? 
+	        			 "rotate({0},{1},{2})".format(d.delta, mid.x, mid.y) : 
+	        			 "rotate({0},{1},{2})".format(d.delta, mid.x, mid.y);
+	      })
+	      .attr("opacity", function(d) {
+	        switch(d.state) {
+	        	case PLANET_STATE.ORBIT:
+	        		return selected == null ? 1 : .05;
+	        	case PLANET_STATE.HOVER:
+	        		return selected == null ? .8 : .3;
+	        	case PLANET_STATE.SELECT:
+	        		return 1;
+	        }
+	      })
+	      .transition().attr("height", function(d) {
+	        //get multiplying factor
+	        if (d.clicked) {
+	         // planetInfo.style("visibility", "visible")
+	        } else {
+	         // planetInfo.style("visibility", "hidden");
+	        }
+	        return d.state != PLANET_STATE.SELECT ? d.radius : height / 2.5;
+	      })
+	      .attr("width", function(d) {
+	        return d.state != PLANET_STATE.SELECT ? d.radius : width / 2.5;
+	      })
+	      .attr("x", function(d) {
+	      	return d.state != PLANET_STATE.SELECT ? mid.x + d.orbit : mid.x - width / 5;
+	      })        
+	      .attr("y", function(d) {
+	      	return d.state != PLANET_STATE.SELECT ? mid.y : mid.y - height / 5;
+	      })
+	      .duration(200);
+
+        svg.selectAll(".orbit").attr("visibility", selected == null ? "visible" : "hidden");
+	  });
 	});
 }
 
